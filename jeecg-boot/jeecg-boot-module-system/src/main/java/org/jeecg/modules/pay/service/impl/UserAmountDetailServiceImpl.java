da3e09425fa885f5565415e9d57b72eacfdd9873
rzh0001 <rzh0001@qq,com>
2019-08-27 22:20:41 +0800

    增加方法：  * 创建收入流水并保存

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/pay/service/impl/UserAmountDetailServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/pay/service/impl/UserAmountDetailServiceImpl.java
@@ -1,11 +1,13 @@
 package org.jeecg.modules.pay.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.jeecg.modules.pay.entity.UserAmountDetail;
 import org.jeecg.modules.pay.mapper.UserAmountDetailMapper;
 import org.jeecg.modules.pay.service.IUserAmountDetailService;
+import org.jeecg.modules.system.entity.SysUser;
 import org.springframework.stereotype.Service;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import java.math.BigDecimal;
 
 /**
  * @Description: 用户收入流水详情
@@ -15,5 +17,20 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
  */
 @Service
 public class UserAmountDetailServiceImpl extends ServiceImpl<UserAmountDetailMapper, UserAmountDetail> implements IUserAmountDetailService {
-
+    
+    @Override
+    public boolean addAmountDetail(BigDecimal amount, String type, SysUser opUser) {
+        UserAmountDetail detail = new UserAmountDetail();
+        detail.setUserId(opUser.getId());
+        detail.setUserName(opUser.getUsername());
+        detail.setType(type);
+        detail.setAmount(amount);
+        detail.setAgentId(opUser.getAgentId());
+        detail.setAgentUsername(opUser.getAgentUsername());
+        detail.setAgentRealname(opUser.getAgentRealname());
+        detail.setSalesmanId(opUser.getSalesmanId());
+        detail.setSalesmanUsername(opUser.getSalesmanUsername());
+        detail.setSalesmanRealname(opUser.getSalesmanRealname());
+        return save(detail);
+    }
 }
