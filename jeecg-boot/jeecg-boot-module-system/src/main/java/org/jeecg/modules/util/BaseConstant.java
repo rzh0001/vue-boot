b8f8d5e59cb5f4b0a6efa854068445acba51c1de
wangjianbin <jianbin.wang@ucarinc.com>
2019-09-20 11:02:49 +0800

    谷歌验证添加白名单

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/util/BaseConstant.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/util/BaseConstant.java
@@ -50,6 +50,7 @@ public class BaseConstant {
     public static final String API_KEY = "apiKey";
     public static final String IP_WHITE_LIST= "ipWhiteList";
     public static final String IP_BLACK_LIST = "ipBlacklist";
+    public static final String SKIP_GOOGLE_CHECK_NAMES = "skipGoogleCheckNames";
     public static final String INNER_CALL_BACK_URL= "innerCallBackUrl";
     public static final String REQUEST_URL = "requestPay";
     public static final String QUERY_ORDER_STATUS_URL = "queryOrderStatusUrl";
