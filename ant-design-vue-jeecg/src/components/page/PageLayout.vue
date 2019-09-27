642ba47dfa29e72f25ed5f8aed0e577c1a842a41
rzh0001 <rzh0001@qq,com>
2019-09-27 23:28:13 +0800

    去掉页面面包屑

--- ant-design-vue-jeecg/src/components/page/PageLayout.vue
+++ ant-design-vue-jeecg/src/components/page/PageLayout.vue
@@ -1,7 +1,8 @@
 <template>
   <div :style="!$route.meta.pageHeader ? 'margin: -10px -24px 0;' : null">
     <!-- pageHeader , route meta hideHeader:true on hide -->
-    <page-header v-if="!$route.meta.pageHeader" :title="title" :logo="logo" :avatar="avatar">
+    <page-header v-if="$route.meta.pageHeader" :title="title" :logo="logo" :avatar="avatar">
+<!--    <page-header v-if="!$route.meta.pageHeader" :title="title" :logo="logo" :avatar="avatar">-->
       <slot slot="action" name="action"></slot>
       <slot slot="content" name="headerContent"></slot>
       <div slot="content" v-if="!this.$slots.headerContent && desc">
