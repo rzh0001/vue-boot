ea42dba858ada3b9d528157e83dc9d0741895343
wangjianbin <1192250933@qq.com>
2019-12-01 19:47:20 +0800

    去除原先旧的php挂马

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/pay/service/requestPayUrl/impl/YsfPayImpl.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/pay/service/requestPayUrl/impl/YsfPayImpl.java
@@ -35,15 +35,16 @@ import java.util.Optional;
 
 @Service
 @Slf4j
-public class YsfPayImpl implements RequestPayUrl<OrderInfoEntity, String, String, String,String, UserBusinessEntity,Object>, InitializingBean {
+public class YsfPayImpl implements RequestPayUrl<OrderInfoEntity, String, String, String, String, UserBusinessEntity, Object>, InitializingBean {
     @Autowired
     private IOrderInfoEntityService orderInfoEntityService;
     @Autowired
     private RedisUtil redisUtil;
     @Autowired
     private RequestUrlUtils utils;
+
     @Override
-    public R requestPayUrl(OrderInfoEntity order, String userName, String url, String key, String callbackUrl,UserBusinessEntity userBusinessEntity) throws Exception {
+    public R requestPayUrl(OrderInfoEntity order, String userName, String url, String key, String callbackUrl, UserBusinessEntity userBusinessEntity) throws Exception {
         String ysfKey = userBusinessEntity.getApiKey();
         if (StringUtils.isBlank(ysfKey)) {
             log.info("云闪付通道未配置apikey");
@@ -56,9 +57,9 @@ public class YsfPayImpl implements RequestPayUrl<OrderInfoEntity, String, String
         }
         String md5Key = keys[0];
         String aesKey = keys[1];
-        String param = structuralYsfParam(order, md5Key, aesKey, order.getBusinessCode(), userName,callbackUrl);
+        String param = structuralYsfParam(order, md5Key, aesKey, order.getBusinessCode(), userName, callbackUrl, userBusinessEntity.getChannelCode());
         String payUrl = ysfCallBack(param, url);
-        if(StringUtils.isEmpty(payUrl)){
+        if (StringUtils.isEmpty(payUrl)) {
             throw new RRException("设备产码失败，请联系商户，查看设置状态");
         }
         redisUtil.del(order.getOuterOrderId());
@@ -66,7 +67,7 @@ public class YsfPayImpl implements RequestPayUrl<OrderInfoEntity, String, String
     }
 
     @Override
-    public boolean orderInfoOk(OrderInfoEntity order, String url,UserBusinessEntity userBusinessEntity) throws Exception {
+    public boolean orderInfoOk(OrderInfoEntity order, String url, UserBusinessEntity userBusinessEntity) throws Exception {
         String apiKey = userBusinessEntity.getApiKey();
         if (StringUtils.isBlank(apiKey)) {
             log.info("云闪付通道未配置apikey");
@@ -112,28 +113,28 @@ public class YsfPayImpl implements RequestPayUrl<OrderInfoEntity, String, String
     }
 
     @Override
-    public boolean notifyOrderFinish(OrderInfoEntity order, String key, UserBusinessEntity userBusiness,String url) throws Exception {
+    public boolean notifyOrderFinish(OrderInfoEntity order, String key, UserBusinessEntity userBusiness, String url) throws Exception {
         JSONObject param = new JSONObject();
-        param.put("orderId",order.getOrderId());
-        log.info("==>手动补单，回调挂马平台url：{}，param:{}",url,param.toJSONString());
+        param.put("orderId", order.getOrderId());
+        log.info("==>手动补单，回调挂马平台url：{}，param:{}", url, param.toJSONString());
         String apiKey = userBusiness.getApiKey();
         String[] keys = apiKey.split("=");
         if (keys.length != 2) {
             log.info("云闪付通道配置apikey规则不对");
-            throw new RRException("云闪付通道未配置apikey,通道为："+order.getPayType());
+            throw new RRException("云闪付通道未配置apikey,通道为：" + order.getPayType());
         }
         String md5Key = keys[0];
         String aesKey = keys[1];
         String data = AES128Util.encryptBase64(param.toJSONString(), aesKey);
         JSONObject requestParam = new JSONObject();
-        requestParam.put("data",data);
-        requestParam.put("businessCode",userBusiness.getBusinessCode());
-        log.info("==>手动补单，回调挂马平台，加密后的入参为：{}",requestParam.toJSONString());
+        requestParam.put("data", data);
+        requestParam.put("businessCode", userBusiness.getBusinessCode());
+        log.info("==>手动补单，回调挂马平台，加密后的入参为：{}", requestParam.toJSONString());
         HttpResult result = HttpUtils.doPostJson(url, requestParam.toJSONString());
-        log.info("==>手动补单，挂马平台返回状态码为：{}；内容为为：{}",result.getCode(),result.getBody());
-        if(result.getCode() == BaseConstant.SUCCESS ){
+        log.info("==>手动补单，挂马平台返回状态码为：{}；内容为为：{}", result.getCode(), result.getBody());
+        if (result.getCode() == BaseConstant.SUCCESS) {
             JSONObject r = JSON.parseObject(result.getBody());
-            if("200".equals(r.get("code").toString())){
+            if ("200".equals(r.get("code").toString())) {
                 return true;
             }
         }
@@ -150,12 +151,27 @@ public class YsfPayImpl implements RequestPayUrl<OrderInfoEntity, String, String
 
 
     private String structuralYsfParam(OrderInfoEntity order, String md5Key, String aesKey, String agentCode,
-                                      String userName,String innerCallBackUrl) {
+                                      String userName, String innerCallBackUrl, String channelCode) {
         Long timestamp = System.currentTimeMillis() / 1000;
         String sign = DigestUtils.md5Hex(agentCode + timestamp + md5Key);
 
         JSONObject reqdata = new JSONObject(true);
-        reqdata.put("orderchannel", 3);
+        int channel = 3;
+        switch (channelCode) {
+            case BaseConstant.REQUEST_ALI_ZZ:
+                channel = 5;
+                break;
+            case BaseConstant.REQUEST_ALI_BANK:
+                channel = 6;
+                break;
+            case BaseConstant.REQUEST_WECHAT_BANK:
+                channel = 7;
+                break;
+            default:
+                channel = 3;
+                break;
+        }
+        reqdata.put("orderchannel", channel);
         reqdata.put("trscode", "dynamicunionpay");
         reqdata.put("agentorderid", order.getOrderId());
         reqdata.put("applyamount", order.getSubmitAmount());
@@ -163,7 +179,7 @@ public class YsfPayImpl implements RequestPayUrl<OrderInfoEntity, String, String
         reqdata.put("clienttype", "H5");
         //这里的回调地址，是挂马回调四方的地址，配置在数据字典中
         reqdata.put("callbackurl", innerCallBackUrl);
-        log.info("===请求云闪付挂码平台的data加密之前的数据为：{}",reqdata.toJSONString());
+        log.info("===请求云闪付挂码平台的data加密之前的数据为：{}", reqdata.toJSONString());
         String data = AES128Util.encryptBase64(reqdata.toJSONString(), aesKey);
         JSONObject reqobj = new JSONObject();
         reqobj.put("agentcode", agentCode);
@@ -172,15 +188,16 @@ public class YsfPayImpl implements RequestPayUrl<OrderInfoEntity, String, String
         reqobj.put("data", data);
         return reqobj.toJSONString();
     }
+
     private String ysfCallBack(String param, String url) throws Exception {
-        log.info("云闪付请求挂马平台，入参为：{}", param);
+        log.info("云闪付请求挂马平台，请求url:{};入参为：{}",url, param);
         if (StringUtils.isBlank(url)) {
             throw new RRException("未配置云闪付回调地址，请联系管理员配置回调地址");
         }
         String resultUrl = null;
-        for(int i=0;i<3;i++){
+        for (int i = 0; i < 3; i++) {
             HttpResult result = HttpUtils.doPostJson(url, param);
-            log.info("==>第：{} 次请求云闪付挂马平台",i);
+            log.info("==>第：{} 次请求云闪付挂马平台", i);
             if (result.getCode() == BaseConstant.SUCCESS) {
                 log.info("云闪付挂马平台返回信息为：{}", result.getBody());
                 JSONObject r = JSON.parseObject(result.getBody());
@@ -195,9 +212,16 @@ public class YsfPayImpl implements RequestPayUrl<OrderInfoEntity, String, String
         }
         return resultUrl;
     }
+
     @Override
     public void afterPropertiesSet() throws Exception {
-        PayServiceFactory.register(BaseConstant.REQUEST_YSF,this);
-        PayServiceFactory.registerUrl(BaseConstant.REQUEST_YSF,utils.getRequestUrl(BaseConstant.REQUEST_YSF));
+        PayServiceFactory.register(BaseConstant.REQUEST_YSF, this);
+        PayServiceFactory.registerUrl(BaseConstant.REQUEST_YSF, utils.getRequestUrl(BaseConstant.REQUEST_YSF));
+        PayServiceFactory.register(BaseConstant.REQUEST_ALI_BANK, this);
+        PayServiceFactory.register(BaseConstant.REQUEST_ALI_ZZ, this);
+        PayServiceFactory.register(BaseConstant.REQUEST_WECHAT_BANK, this);
+        PayServiceFactory.registerUrl(BaseConstant.REQUEST_ALI_BANK, utils.getRequestUrl(BaseConstant.REQUEST_ALI_BANK));
+        PayServiceFactory.registerUrl(BaseConstant.REQUEST_ALI_ZZ, utils.getRequestUrl(BaseConstant.REQUEST_ALI_ZZ));
+        PayServiceFactory.registerUrl(BaseConstant.REQUEST_WECHAT_BANK, utils.getRequestUrl(BaseConstant.REQUEST_WECHAT_BANK));
     }
 }
