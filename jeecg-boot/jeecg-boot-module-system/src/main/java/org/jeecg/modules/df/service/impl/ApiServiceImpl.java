1fd7d0b89a9dc226c019d1168aca6cc70a3b3bed
ruanzh <rzh0001@qq.com>
2019-12-02 22:17:18 +0800

    优化接口日志

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/df/service/impl/ApiServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/df/service/impl/ApiServiceImpl.java
@@ -29,130 +29,130 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ApiServiceImpl implements IApiService {
 
-    @Autowired
-    private ISysUserService userService;
+  @Autowired private ISysUserService userService;
 
-    @Autowired
-    private IUserAmountEntityService amountService;
+  @Autowired private IUserAmountEntityService amountService;
 
-    @Autowired
-    private IPayOrderService payOrderService;
+  @Autowired private IPayOrderService payOrderService;
 
-    @Override
-    @Transactional(rollbackFor = Exception.class)
-    public ApiResponseBody<PayOrderResult> createOrder(ApiRequestBody req) {
-
-        // 检查商户状态
-        SysUser user = userService.getUserByName(req.getUsername());
-        if (BeanUtil.isEmpty(user)) {
-            throw new AccountAbnormalException("商户不存在，请检查参数");
-        }
-
-        // 验签
-        if (!req.verifySignature(user.getApiKey())) {
-            throw new SignatureException("签名失败");
-        }
-
-        if (!CommonConstant.USER_UNFREEZE.equals(user.getStatus())) {
-            throw new AccountAbnormalException("商户状态异常，请联系管理员！");
-        }
-
-        // 检查代理状态
-        SysUser agent = userService.getUserById(user.getAgentId());
-        if (!CommonConstant.USER_UNFREEZE.equals(agent.getStatus())) {
-            throw new AccountAbnormalException("商户上级代理状态异常，请联系管理员！");
-        }
-
-        // 解密数据
-        String data = req.decodeData(user.getApiKey());
-        cn.hutool.json.JSONObject jsonObject = JSONUtil.parseObj(data);
-        PayOrderData payOrderData = jsonObject.toBean(PayOrderData.class);
-
-
-        // 检查订单信息
-
-        PayOrder order = payOrderData.toPayOrder(user);
-        PayOrderResult result = payOrderService.apiOrder(order);
-        log.info(result.toJsonString());
-        ApiResponseBody resp = ApiResponseBody.ok();
-        resp.setData(result);
-        return resp;
+  @Override
+  @Transactional(rollbackFor = Exception.class)
+  public ApiResponseBody<PayOrderResult> createOrder(ApiRequestBody req) {
+    log.info("\n=======>调用订单创建接口：检查参数");
+    // 检查商户状态
+    SysUser user = userService.getUserByName(req.getUsername());
+    if (BeanUtil.isEmpty(user)) {
+      log.error("\n=======>调用订单创建接口：商户不存在，请检查参数");
+      throw new AccountAbnormalException("商户不存在，请检查参数");
     }
 
-    @Override
-    public ApiResponseBody queryOrder(ApiRequestBody req) {
-        // 检查商户状态
-        SysUser user = userService.getUserByName(req.getUsername());
-        if (BeanUtil.isEmpty(user)) {
-            throw new AccountAbnormalException("商户不存在，请检查参数");
-        }
+    // 验签
+    if (!req.verifySignature(user.getApiKey())) {
+      log.error("\n=======>调用订单创建接口：签名失败");
+      throw new SignatureException("签名失败");
+    }
+    log.info("\n=======>调用订单创建接口：验签成功");
 
-        // 验签
-        if (!req.verifySignature(user.getApiKey())) {
-            throw new SignatureException("签名失败");
-        }
-
-        if (!CommonConstant.USER_UNFREEZE.equals(user.getStatus())) {
-            throw new AccountAbnormalException("商户状态异常，请联系管理员！");
-        }
-
-        // 检查代理状态
-        SysUser agent = userService.getUserById(user.getAgentId());
-        if (!CommonConstant.USER_UNFREEZE.equals(agent.getStatus())) {
-            throw new AccountAbnormalException("商户上级代理状态异常，请联系管理员！");
-        }
-
-        // 解密数据
-        String data = req.decodeData(user.getApiKey());
-        cn.hutool.json.JSONObject jsonObject = JSONUtil.parseObj(data);
-        QueryOrderData queryOrderData = jsonObject.toBean(QueryOrderData.class);
-
-        PayOrder order = payOrderService.getByOuterOrderId(queryOrderData.getBizOrderNo());
-        if (BeanUtil.isEmpty(order)) {
-            throw new ApiException(1007, "查无此订单[outerOrderId:" + queryOrderData.getBizOrderNo() + "]");
-        }
-        PayOrderResult result = PayOrderResult.fromPayOrder(order);
-        ApiResponseBody resp = ApiResponseBody.ok();
-        resp.setData(result);
-        return resp;
+    if (!CommonConstant.USER_UNFREEZE.equals(user.getStatus())) {
+      log.error("\n=======>调用订单创建接口：商户状态异常，请联系管理员！");
+      throw new AccountAbnormalException("商户状态异常，请联系管理员！");
     }
 
-    @Override
-    public boolean callback(String orderId) {
-        PayOrder order = payOrderService.getById(orderId);
-        if (!order.getStatus().equals("2") && !order.getStatus().equals("3")) {
-            throw new RRException("订单未完结，不允许回调");
-        }
+    // 检查代理状态
+    SysUser agent = userService.getUserById(user.getAgentId());
+    if (!CommonConstant.USER_UNFREEZE.equals(agent.getStatus())) {
+      log.error("\n=======>调用订单创建接口：商户上级代理状态异常，请联系管理员！");
+      throw new AccountAbnormalException("商户上级代理状态异常，请联系管理员！");
+    }
+    // 解密数据
+    String data = req.decodeData(user.getApiKey());
+    JSONObject jsonObject = JSONUtil.parseObj(data);
+    PayOrderData payOrderData = jsonObject.toBean(PayOrderData.class);
+    log.info("\n=======>订单[{}]：解密成功", payOrderData.getBizOrderNo());
 
-        SysUser user = userService.getById(order.getUserId());
+    // 检查订单信息
+    PayOrder order = payOrderData.toPayOrder(user);
+    PayOrderResult result = payOrderService.apiOrder(order);
+    log.info(result.toJsonString());
+    log.info("\n=======>订单[{}]：创建成功", order.getOuterOrderId());
+    ApiResponseBody resp = ApiResponseBody.ok();
+    resp.setData(result);
+    return resp;
+  }
 
-        PayOrderResult result = PayOrderResult.fromPayOrder(order);
-        CallbackBody body = new CallbackBody();
-        body.setUsername(user.getUsername());
-        body.setTimestamp(System.currentTimeMillis());
-        body.setData(result.encodeData(user.getApiKey()));
-        body.setSign(body.sign(user.getApiKey()));
-
-        String post = HttpUtil.post(order.getCallbackUrl(), body.toJsonString());
-        // 先更新为已发送未返回
-        order.setCallbackStatus("1");
-        payOrderService.updateById(order);
-
-        log.info("=======>异步回调返回报文:{}", post);
-
-        JSONObject jsonObject = JSONUtil.parseObj(post);
-        if (BeanUtil.isEmpty(jsonObject)) {
-            throw new RRException("解析返回数据出错！请联系管理员！");
-        }
-
-        int code = (int) jsonObject.get("code");
-        if (code == 0) {
-            order.setCallbackStatus("2");
-            payOrderService.updateById(order);
-        }
-
-        return true;
+  @Override
+  public ApiResponseBody queryOrder(ApiRequestBody req) {
+    // 检查商户状态
+    SysUser user = userService.getUserByName(req.getUsername());
+    if (BeanUtil.isEmpty(user)) {
+      throw new AccountAbnormalException("商户不存在，请检查参数");
     }
 
+    // 验签
+    if (!req.verifySignature(user.getApiKey())) {
+      throw new SignatureException("签名失败");
+    }
 
+    if (!CommonConstant.USER_UNFREEZE.equals(user.getStatus())) {
+      throw new AccountAbnormalException("商户状态异常，请联系管理员！");
+    }
+
+    // 检查代理状态
+    SysUser agent = userService.getUserById(user.getAgentId());
+    if (!CommonConstant.USER_UNFREEZE.equals(agent.getStatus())) {
+      throw new AccountAbnormalException("商户上级代理状态异常，请联系管理员！");
+    }
+
+    // 解密数据
+    String data = req.decodeData(user.getApiKey());
+    cn.hutool.json.JSONObject jsonObject = JSONUtil.parseObj(data);
+    QueryOrderData queryOrderData = jsonObject.toBean(QueryOrderData.class);
+
+    PayOrder order = payOrderService.getByOuterOrderId(queryOrderData.getBizOrderNo());
+    if (BeanUtil.isEmpty(order)) {
+      throw new ApiException(1007, "查无此订单[outerOrderId:" + queryOrderData.getBizOrderNo() + "]");
+    }
+    PayOrderResult result = PayOrderResult.fromPayOrder(order);
+    ApiResponseBody resp = ApiResponseBody.ok();
+    resp.setData(result);
+    return resp;
+  }
+
+  @Override
+  public boolean callback(String orderId) {
+    PayOrder order = payOrderService.getById(orderId);
+    if (!order.getStatus().equals("2") && !order.getStatus().equals("3")) {
+      throw new RRException("订单未完结，不允许回调");
+    }
+
+    SysUser user = userService.getById(order.getUserId());
+
+    PayOrderResult result = PayOrderResult.fromPayOrder(order);
+    CallbackBody body = new CallbackBody();
+    body.setUsername(user.getUsername());
+    body.setTimestamp(System.currentTimeMillis());
+    body.setData(result.encodeData(user.getApiKey()));
+    body.setSign(body.sign(user.getApiKey()));
+
+    log.info("\n=======>订单[{}]发送异步回调", order.getOuterOrderId());
+    String post = HttpUtil.post(order.getCallbackUrl(), body.toJsonString());
+    // 先更新为已发送未返回
+    order.setCallbackStatus("1");
+    payOrderService.updateById(order);
+
+    log.info("\n=======>订单[{}]异步回调返回报文:{}", order.getOuterOrderId(), post);
+
+    JSONObject jsonObject = JSONUtil.parseObj(post);
+    if (BeanUtil.isEmpty(jsonObject)) {
+      throw new RRException("解析返回数据出错！请联系管理员！");
+    }
+
+    int code = (int) jsonObject.get("code");
+    if (code == 0) {
+      order.setCallbackStatus("2");
+      payOrderService.updateById(order);
+    }
+
+    return true;
+  }
 }
--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/df/service/impl/PayOrderServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/df/service/impl/PayOrderServiceImpl.java
@@ -1,8 +1,10 @@
 package org.jeecg.modules.df.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.SecurityUtils;
 import org.jeecg.common.system.vo.LoginUser;
 import org.jeecg.modules.df.constant.DfConstant;
@@ -25,149 +27,173 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.util.Map;
 
-/**
- * @Description: 1
- * @Author: jeecg-boot
- * @Date: 2019-10-27
- * @Version: V1.0
- */
+@Slf4j
 @Service
-public class PayOrderServiceImpl extends ServiceImpl<PayOrderMapper, PayOrder> implements IPayOrderService {
+public class PayOrderServiceImpl extends ServiceImpl<PayOrderMapper, PayOrder>
+    implements IPayOrderService {
 
-    @Autowired
-    private ISysUserService userService;
+  @Autowired private ISysUserService userService;
 
-    @Autowired
-    private IUserAmountEntityService userAmountService;
+  @Autowired private IUserAmountEntityService userAmountService;
 
-    @Autowired
-    @Lazy
-    private IApiService apiService;
+  @Autowired @Lazy private IApiService apiService;
 
-    @Override
-    @Transactional(rollbackFor = Exception.class)
-    public boolean create(PayOrder order) {
-        LoginUser ou = (LoginUser) SecurityUtils.getSubject().getPrincipal();
+  @Override
+  @Transactional(rollbackFor = Exception.class)
+  public boolean create(PayOrder order) {
+    LoginUser ou = (LoginUser) SecurityUtils.getSubject().getPrincipal();
 
-
-        // 获取费率配置
-        SysUser user = userService.getById(ou.getId());
-        if (BeanUtil.isEmpty(user) || null == user.getTransactionFeeRate() || null == user.getOrderFixedFee()) {
-            throw new RRException("获取费率配置失败！");
-        }
-
-        order.setUserId(user.getId());
-        order.setUserName(user.getUsername());
-        order.setUserRealname(user.getRealname());
-        order.setAgentId(user.getAgentId());
-        order.setAgentUsername(user.getAgentUsername());
-        order.setAgentRealname(user.getAgentRealname());
-
-        // 生成订单号
-        order.setOrderId(IDUtil.genPayOrderId());
-        order.setStatus(DfConstant.STATUS_SAVE);
-
-        // 计算手续费
-        order.setTransactionFee(order.getAmount().multiply(user.getTransactionFeeRate()).setScale(2, BigDecimal.ROUND_HALF_UP));
-        order.setFixedFee(user.getOrderFixedFee());
-        order.setOrderFee(order.getTransactionFee().add(order.getFixedFee()));
-
-        // 检查代付额度
-        BigDecimal userAmount = userAmountService.getUserAmount(ou.getId());
-        if (userAmount.compareTo(order.getAmount().add(order.getOrderFee())) < 0) {
-            throw new RRException("余额不足，请及时充值！本订单需扣减[" + order.getAmount().add(order.getOrderFee()) + "]元");
-        }
-        // 扣减商户余额
-        String remark = "单笔固定手续费：" + order.getFixedFee() + "，交易手续费：" + order.getTransactionFee();
-        userAmountService.changeAmount(user.getId(), order.getAmount().negate(), order.getOrderId(), "", "5");
-        userAmountService.changeAmount(user.getId(), order.getOrderFee().negate(), order.getOrderId(), remark, "6");
-
-        return save(order);
+    // 获取费率配置
+    SysUser user = userService.getById(ou.getId());
+    if (BeanUtil.isEmpty(user)
+        || null == user.getTransactionFeeRate()
+        || null == user.getOrderFixedFee()) {
+      throw new RRException("获取费率配置失败！");
     }
 
-    @Override
-    @Transactional(rollbackFor = Exception.class)
-    public boolean checked(PayOrder order) {
+    order.setUserId(user.getId());
+    order.setUserName(user.getUsername());
+    order.setUserRealname(user.getRealname());
+    order.setAgentId(user.getAgentId());
+    order.setAgentUsername(user.getAgentUsername());
+    order.setAgentRealname(user.getAgentRealname());
 
-        // 修改订单状态
-        updateById(order);
+    // 生成订单号
+    order.setOrderId(IDUtil.genPayOrderId());
+    order.setStatus(DfConstant.STATUS_SAVE);
 
-        // 增加代理收入
-        // 生成收入明细
-        String remark = "单笔固定手续费：" + order.getFixedFee() + "，交易手续费：" + order.getTransactionFee();
-        userAmountService.changeAmount(order.getAgentId(), order.getOrderFee(), order.getOrderId(), remark, "1");
-//        apiService.callback(order.getId());
-        return false;
+    // 计算手续费
+    order.setTransactionFee(
+        order
+            .getAmount()
+            .multiply(user.getTransactionFeeRate())
+            .setScale(2, BigDecimal.ROUND_HALF_UP));
+    order.setFixedFee(user.getOrderFixedFee());
+    order.setOrderFee(order.getTransactionFee().add(order.getFixedFee()));
+
+    // 检查代付额度
+    BigDecimal userAmount = userAmountService.getUserAmount(ou.getId());
+    if (userAmount.compareTo(order.getAmount().add(order.getOrderFee())) < 0) {
+      throw new RRException(
+          "余额不足，请及时充值！本订单需扣减[" + order.getAmount().add(order.getOrderFee()) + "]元");
+    }
+    // 扣减商户余额
+    String remark = "单笔固定手续费：" + order.getFixedFee() + "，交易手续费：" + order.getTransactionFee();
+    userAmountService.changeAmount(
+        user.getId(), order.getAmount().negate(), order.getOrderId(), "", "5");
+    userAmountService.changeAmount(
+        user.getId(), order.getOrderFee().negate(), order.getOrderId(), remark, "6");
+
+    return save(order);
+  }
+
+  @Override
+  @Transactional(rollbackFor = Exception.class)
+  public boolean checked(PayOrder order) {
+
+    // 修改订单状态
+    updateById(order);
+
+    // 增加代理收入
+    // 生成收入明细
+    String remark = "单笔固定手续费：" + order.getFixedFee() + "，交易手续费：" + order.getTransactionFee();
+    userAmountService.changeAmount(
+        order.getAgentId(), order.getOrderFee(), order.getOrderId(), remark, "1");
+    if (StrUtil.isNotBlank(order.getCallbackUrl())) {
+      apiService.callback(order.getId());
+    }
+    return true;
+  }
+
+  @Override
+  @Transactional(rollbackFor = Exception.class)
+  public boolean rejected(PayOrder order) {
+
+    // 修改订单状态
+    updateById(order);
+
+    // 返还商户额度
+    // 生成收入明细
+    userAmountService.changeAmount(
+        order.getUserId(), order.getAmount(), order.getOrderId(), order.getRemark(), "3");
+    userAmountService.changeAmount(
+        order.getUserId(), order.getOrderFee(), order.getOrderId(), order.getRemark(), "3");
+    if (StrUtil.isNotBlank(order.getCallbackUrl())) {
+      apiService.callback(order.getId());
+    }
+    return true;
+  }
+
+  @Override
+  public PayOrderResult apiOrder(PayOrder order) {
+
+    int count = baseMapper.count(order.getUserId(), order.getOuterOrderId());
+    if (count > 0) {
+      log.info("\n=======>订单[{}]：订单号重复", order.getOuterOrderId());
+      throw new ApiException(1005, "订单号重复");
     }
 
-    @Override
-    @Transactional(rollbackFor = Exception.class)
-    public boolean rejected(PayOrder order) {
-
-        // 修改订单状态
-        updateById(order);
-
-        // 返还商户额度
-        // 生成收入明细
-        userAmountService.changeAmount(order.getUserId(), order.getAmount(), order.getOrderId(), order.getRemark(), "3");
-        userAmountService.changeAmount(order.getUserId(), order.getOrderFee(), order.getOrderId(), order.getRemark(), "3");
-        apiService.callback(order.getId());
-        return false;
+    // 获取费率配置
+    SysUser user = userService.getById(order.getUserId());
+    if (BeanUtil.isEmpty(user)
+        || null == user.getTransactionFeeRate()
+        || null == user.getOrderFixedFee()) {
+      log.info("\n=======>订单[{}]：获取费率配置失败", order.getOuterOrderId());
+      throw new ApiException(1006, "获取费率配置失败！");
     }
 
-    @Override
-    public PayOrderResult apiOrder(PayOrder order) {
+    // 生成订单号
+    order.setOrderId(IDUtil.genPayOrderId());
+    order.setStatus(DfConstant.STATUS_SAVE);
+    order.setCallbackStatus("0");
 
-        int count = baseMapper.count(order.getUserId(), order.getOuterOrderId());
-        if (count > 0) {
-            throw new ApiException(1005, "订单号重复");
-        }
+    // 计算手续费
+    order.setTransactionFee(
+        order
+            .getAmount()
+            .multiply(user.getTransactionFeeRate())
+            .setScale(2, BigDecimal.ROUND_HALF_UP));
+    order.setFixedFee(user.getOrderFixedFee());
+    order.setOrderFee(order.getTransactionFee().add(order.getFixedFee()));
 
-
-        // 获取费率配置
-        SysUser user = userService.getById(order.getUserId());
-        if (BeanUtil.isEmpty(user) || null == user.getTransactionFeeRate() || null == user.getOrderFixedFee()) {
-            throw new ApiException(1006, "获取费率配置失败！");
-        }
-
-        // 生成订单号
-        order.setOrderId(IDUtil.genPayOrderId());
-        order.setStatus(DfConstant.STATUS_SAVE);
-        order.setCallbackStatus("0");
-
-        // 计算手续费
-        order.setTransactionFee(order.getAmount().multiply(user.getTransactionFeeRate()).setScale(2, BigDecimal.ROUND_HALF_UP));
-        order.setFixedFee(user.getOrderFixedFee());
-        order.setOrderFee(order.getTransactionFee().add(order.getFixedFee()));
-
-        // 检查代付额度
-        BigDecimal userAmount = userAmountService.getUserAmount(order.getUserId());
-        if (userAmount.compareTo(order.getAmount().add(order.getOrderFee())) < 0) {
-            throw new ApiException(1004, "余额不足，请及时充值！当前余额[" + userAmount + "]元，本订单需[" + order.getAmount().add(order.getOrderFee()) + "]元");
-        }
-        // 扣减商户余额
-        userAmountService.changeAmount(user.getId(), order.getAmount().negate(), order.getOrderId(), null, "5");
-        String remark = "单笔固定手续费：" + order.getFixedFee() + "，交易手续费：" + order.getTransactionFee();
-        userAmountService.changeAmount(user.getId(), order.getOrderFee().negate(), order.getOrderId(), remark, "6");
-
-        save(order);
-        PayOrderResult result = PayOrderResult.fromPayOrder(order);
-        return result;
+    // 检查代付额度
+    BigDecimal userAmount = userAmountService.getUserAmount(order.getUserId());
+    if (userAmount.compareTo(order.getAmount().add(order.getOrderFee())) < 0) {
+      log.info("\n=======>订单[{}]：余额不足，请及时充值！", order.getOuterOrderId());
+      throw new ApiException(
+          1004,
+          "余额不足，请及时充值！当前余额["
+              + userAmount
+              + "]元，本订单需["
+              + order.getAmount().add(order.getOrderFee())
+              + "]元");
     }
+    // 扣减商户余额
+    userAmountService.changeAmount(
+        user.getId(), order.getAmount().negate(), order.getOrderId(), null, "5");
+    String remark = "单笔固定手续费：" + order.getFixedFee() + "，交易手续费：" + order.getTransactionFee();
+    userAmountService.changeAmount(
+        user.getId(), order.getOrderFee().negate(), order.getOrderId(), remark, "6");
+    log.info("\n=======>订单[{}]：扣除手续费成功", order.getOuterOrderId());
 
-    @Override
-    public int count(String userId, String outerOrderId) {
+    save(order);
+    PayOrderResult result = PayOrderResult.fromPayOrder(order);
+    return result;
+  }
 
-        return baseMapper.count(userId, outerOrderId);
-    }
+  @Override
+  public int count(String userId, String outerOrderId) {
 
-    @Override
-    public PayOrder getByOuterOrderId(String outerOrderId) {
-        return baseMapper.getByOuterOrderId(outerOrderId);
-    }
+    return baseMapper.count(userId, outerOrderId);
+  }
 
-    @Override
-    public Map<String, Object> summary(QueryWrapper<PayOrder> queryWrapper) {
-        return baseMapper.summary(queryWrapper);
-    }
+  @Override
+  public PayOrder getByOuterOrderId(String outerOrderId) {
+    return baseMapper.getByOuterOrderId(outerOrderId);
+  }
+
+  @Override
+  public Map<String, Object> summary(QueryWrapper<PayOrder> queryWrapper) {
+    return baseMapper.summary(queryWrapper);
+  }
 }
