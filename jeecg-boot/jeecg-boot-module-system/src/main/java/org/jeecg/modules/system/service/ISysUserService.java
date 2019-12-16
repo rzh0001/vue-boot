2c6f0bfbf32d6d1af4b18ce47d5c2321d087df0d
ruanzh <rzh0001@qq.com>
2019-12-17 00:26:29 +0800

    修复查询条件；此处查询SQL比较特别，多表联合查询 可以借鉴

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/service/ISysUserService.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/service/ISysUserService.java
@@ -1,5 +1,6 @@
 package org.jeecg.modules.system.service;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
@@ -23,34 +24,41 @@ import java.util.Set;
  */
 public interface ISysUserService extends IService<SysUser> {
 	void cleanGoogle(String username);
-	void updateUserGoogleKey(String userName,String googleKey);
+
+	void updateUserGoogleKey(String userName, String googleKey);
+
 	public SysUser getUserByName(String username);
 
 	public SysUser getUserById(String id);
+
 	/**
 	 * 添加用户和用户角色关系
+	 *
 	 * @param user
 	 * @param roles
 	 */
-	public void addUserWithRole(SysUser user,String roles);
-	
-	
+	public void addUserWithRole(SysUser user, String roles);
+
+
 	/**
 	 * 修改用户和用户角色关系
+	 *
 	 * @param user
 	 * @param roles
 	 */
-	public void editUserWithRole(SysUser user,String roles);
+	public void editUserWithRole(SysUser user, String roles);
 
 	/**
 	 * 获取用户的授权角色
+	 *
 	 * @param username
 	 * @return
 	 */
 	public List<String> getRole(String username);
-	
+
 	/**
-	  * 查询用户信息包括 部门信息
+	 * 查询用户信息包括 部门信息
+	 *
 	 * @param username
 	 * @return
 	 */
@@ -58,6 +66,7 @@ public interface ISysUserService extends IService<SysUser> {
 
 	/**
 	 * 根据部门Id查询
+	 *
 	 * @param
 	 * @return
 	 */
@@ -65,10 +74,11 @@ public interface ISysUserService extends IService<SysUser> {
 
 	/**
 	 * 根据角色Id查询
+	 *
 	 * @param
 	 * @return
 	 */
-	public IPage<SysUser> getUserByRoleId(Page<SysUser> page,String roleId, String username);
+	public IPage<SysUser> getUserByRoleId(Page<SysUser> page, String roleId, String username);
 
 	/**
 	 * 通过用户名获取用户角色集合
@@ -85,14 +95,15 @@ public interface ISysUserService extends IService<SysUser> {
 	 * @return 权限集合
 	 */
 	Set<String> getUserPermissionsSet(String username);
-	
+
 	/**
 	 * 根据用户名设置部门ID
+	 *
 	 * @param username
 	 * @param orgCode
 	 */
-	void updateUserDepart(String username,String orgCode);
-	
+	void updateUserDepart(String username, String orgCode);
+
 	/**
 	 * 根据手机号获取用户名和密码
 	 */
@@ -107,6 +118,7 @@ public interface ISysUserService extends IService<SysUser> {
 
 	/**
 	 * 添加用户和用户部门关系
+	 *
 	 * @param user
 	 * @param selectedParts
 	 */
@@ -114,22 +126,25 @@ public interface ISysUserService extends IService<SysUser> {
 
 	/**
 	 * 编辑用户和用户部门关系
+	 *
 	 * @param user
 	 * @param departs
 	 */
 	void editUserWithDepart(SysUser user, String departs);
-	
+
 	/**
-	   * 校验用户是否有效
+	 * 校验用户是否有效
+	 *
 	 * @param sysUser
 	 * @return
 	 */
 	Result checkUserIsEffective(SysUser sysUser);
-	
+
 	public void addPayMember(SysUser user, String id);
 
 	/**
 	 * 通过代理获取代理下面的所有商户
+	 *
 	 * @param agentName
 	 * @return
 	 */
@@ -138,21 +153,21 @@ public interface ISysUserService extends IService<SysUser> {
 	List<SysUser> getUserAndReferByAgent(String agentName);
 
 	List<String> getUserByRefer(String refer);
-	
+
 	/**
 	 * 翻页查询
 	 *
 	 * @param page 翻页对象
 	 * @param map
 	 */
-	IPage<SysUserPage> pageUserWithPaymentInfo(Page page, Map<String, Object> map);
-    
-    /**
-     * 手工调账
-     *
-     * @param username
-     * @param adjustAmount
-     * @return
-     */
+	IPage<SysUserPage> pageUserWithPaymentInfo(Page page, Wrapper wrapper);
+
+	/**
+	 * 手工调账
+	 *
+	 * @param username
+	 * @param adjustAmount
+	 * @return
+	 */
 	boolean adjustAmount(String username, BigDecimal adjustAmount, String remark);
 }
