7cec180b4918dea2b6a43868803d9a99c31411ab
rzh0001@qq.com <rzh0001@qq.com>
2020-03-01 22:38:11 +0800

    修复 首页管理员数据汇总问题

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/mapper/DashboardMapper.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/mapper/DashboardMapper.java
@@ -9,57 +9,67 @@ import java.util.Map;
 
 public interface DashboardMapper extends BaseMapper {
 
-  /**
-   * 统计代理下用户的总可用额度
-   *
-   * @param agentId
-   * @return
-   */
-  @Select("select sum(amount) from sys_user_amount where agent_id = #{agentId}")
-  BigDecimal summaryUserAmount(@Param("agentId") String agentId);
 
-  @Select("select sum(amount) from df_pay_order where agent_id = #{agentId} and status = '2'")
-  BigDecimal summaryDfPayAmount(@Param("agentId") String agentId);
+	@Select("select sum(amount) from sys_user_amount")
+	BigDecimal summaryUserAmount();
 
-  @Select("select sum(order_fee) from df_pay_order where agent_id = #{agentId} and status = '2'")
-  BigDecimal summaryDfOrderFee(@Param("agentId") String agentId);
+	@Select("select sum(amount) from sys_user_amount where agent_id = #{agentId}")
+	BigDecimal summaryUserAmount(@Param("agentId") String agentId);
 
-  @Select(
-      "select sum(amount) from df_recharge_order where agent_id = #{agentId} "
-          + "and status = '2' and to_days(create_time) = to_days(now())")
-  BigDecimal summaryTodayRechargeAmount(@Param("agentId") String agentId);
+	@Select("select sum(amount) from df_pay_order where status = '2'")
+	BigDecimal summaryDfPayAmount();
 
-  @Select(
-      "select sum(amount) from df_pay_order where agent_id = #{agentId} "
-          + "and status = '2' and to_days(create_time) = to_days(now())")
-  BigDecimal summaryTodayDfPayAmount(@Param("agentId") String agentId);
+	@Select("select sum(amount) from df_pay_order where agent_id = #{agentId} and status = '2'")
+	BigDecimal summaryDfPayAmount(@Param("agentId") String agentId);
 
-  @Select(
-      "select sum(order_fee) from df_pay_order where agent_id = #{agentId} "
-          + "and status = '2' and to_days(create_time) = to_days(now())")
-  BigDecimal summaryTodayDfOrderFee(@Param("agentId") String agentId);
+	@Select("select sum(order_fee) from df_pay_order where status = '2'")
+	BigDecimal summaryDfOrderFee();
 
-  @Select("select amount from sys_user_amount where user_id = #{userId}")
-  BigDecimal getUserAmount(@Param("userId") String userId);
+	@Select("select sum(order_fee) from df_pay_order where agent_id = #{agentId} and status = '2'")
+	BigDecimal summaryDfOrderFee(@Param("agentId") String agentId);
 
-  @Select("select sum(amount) from df_pay_order where user_id = #{userId} and status = '2'")
-  BigDecimal summaryUserDfPayAmount(@Param("userId") String userId);
+	@Select("select sum(amount) from df_recharge_order where status = '2' and to_days(create_time) = to_days(now())")
+	BigDecimal summaryTodayRechargeAmount();
 
-  @Select("select sum(order_fee) from df_pay_order where user_id = #{userId} and status = '2'")
-  BigDecimal summaryUserDfOrderFee(@Param("userId") String userId);
+	@Select("select sum(amount) from df_recharge_order where agent_id = #{agentId} "
+			+ "and status = '2' and to_days(create_time) = to_days(now())")
+	BigDecimal summaryTodayRechargeAmount(@Param("agentId") String agentId);
 
-  @Select(
-      "select sum(amount) from df_recharge_order where user_id = #{userId} "
-          + "and status = '2' and to_days(create_time) = to_days(now())")
-  BigDecimal summaryUserTodayRechargeAmount(@Param("userId") String userId);
+	@Select("select sum(amount) from df_pay_order where status = '2' and to_days(create_time) = to_days(now())")
+	BigDecimal summaryTodayDfPayAmount();
 
-  @Select(
-      "select sum(amount) from df_pay_order where user_id = #{userId} "
-          + "and status = '2' and to_days(create_time) = to_days(now())")
-  BigDecimal summaryUserTodayDfPayAmount(@Param("userId") String userId);
+	@Select("select sum(amount) from df_pay_order where agent_id = #{agentId} "
+			+ "and status = '2' and to_days(create_time) = to_days(now())")
+	BigDecimal summaryTodayDfPayAmount(@Param("agentId") String agentId);
 
-  @Select(
-      "select sum(order_fee) from df_pay_order where user_id = #{userId} "
-          + "and status = '2' and to_days(create_time) = to_days(now())")
-  BigDecimal summaryUserTodayDfOrderFee(@Param("userId") String userId);
+	@Select("select sum(order_fee) from df_pay_order where status = '2' and to_days(create_time) = to_days(now())")
+	BigDecimal summaryTodayDfOrderFee();
+
+	@Select("select sum(order_fee) from df_pay_order where agent_id = #{agentId} "
+			+ "and status = '2' and to_days(create_time) = to_days(now())")
+	BigDecimal summaryTodayDfOrderFee(@Param("agentId") String agentId);
+
+	@Select("select amount from sys_user_amount where user_id = #{userId}")
+	BigDecimal getUserAmount(@Param("userId") String userId);
+
+	@Select("select sum(amount) from df_pay_order where user_id = #{userId} and status = '2'")
+	BigDecimal summaryUserDfPayAmount(@Param("userId") String userId);
+
+	@Select("select sum(order_fee) from df_pay_order where user_id = #{userId} and status = '2'")
+	BigDecimal summaryUserDfOrderFee(@Param("userId") String userId);
+
+	@Select(
+			"select sum(amount) from df_recharge_order where user_id = #{userId} "
+					+ "and status = '2' and to_days(create_time) = to_days(now())")
+	BigDecimal summaryUserTodayRechargeAmount(@Param("userId") String userId);
+
+	@Select(
+			"select sum(amount) from df_pay_order where user_id = #{userId} "
+					+ "and status = '2' and to_days(create_time) = to_days(now())")
+	BigDecimal summaryUserTodayDfPayAmount(@Param("userId") String userId);
+
+	@Select(
+			"select sum(order_fee) from df_pay_order where user_id = #{userId} "
+					+ "and status = '2' and to_days(create_time) = to_days(now())")
+	BigDecimal summaryUserTodayDfOrderFee(@Param("userId") String userId);
 }
--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/service/impl/DashboardServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/service/impl/DashboardServiceImpl.java
@@ -23,77 +23,106 @@ import java.util.Map;
 @Service
 public class DashboardServiceImpl extends IBaseService implements IDashboardService {
 
-  @Autowired private ISysUserService userService;
+	@Autowired
+	private ISysUserService userService;
 
-  @Autowired private IUserAmountEntityService amountService;
+	@Autowired
+	private IUserAmountEntityService amountService;
 
-  @Autowired private IUserAmountDetailService amountDetailService;
+	@Autowired
+	private IUserAmountDetailService amountDetailService;
 
-  @Autowired private DashboardMapper mapper;
+	@Autowired
+	private DashboardMapper mapper;
 
-  @Override
-  public Result<Map<String, Object>> homepageSummary() {
-    Result r = new Result();
-    LoginUser loginUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
-    SysUser user = userService.getUserById(loginUser.getId());
+	@Override
+	public Result<Map<String, Object>> homepageSummary() {
+		Result r = new Result();
+		LoginUser loginUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
+		SysUser user = userService.getUserById(loginUser.getId());
+		Map<String, Object> summary = null;
+		if (user.getMemberType() == null) {
+			summary = adminSummary();
+		} else if (user.getMemberType().equals("1")) {
+			summary = agentSummary(user.getId());
+			r.setSuccess(true);
+		} else if (user.getMemberType().equals("3")) {
+			summary = userSummary(user.getId());
+			r.setSuccess(true);
+		}
+		r.setResult(summary);
 
-    if (user.getMemberType().equals("1")) {
-      Map<String, Object> summary = agentSummary(user.getId());
-      r.setSuccess(true);
-      r.setResult(summary);
-    } else if (user.getMemberType().equals("3")) {
-      Map<String, Object> summary = userSummary(user.getId());
-      r.setSuccess(true);
-      r.setResult(summary);
-    }
+		return r;
+	}
 
-    return r;
-  }
+	public Map<String, Object> adminSummary() {
+		Map map = new HashMap();
+		// 剩余额度
+		BigDecimal userAmount = mapper.summaryUserAmount();
+		// 已代付总额
+		BigDecimal payAmount = mapper.summaryDfPayAmount();
+		// 手续费收入
+		BigDecimal orderFee = mapper.summaryDfOrderFee();
+		// 今日充值
+		BigDecimal todayRechargeAmount = mapper.summaryTodayRechargeAmount();
+		// 今日代付
+		BigDecimal todayDfPayAmount = mapper.summaryTodayDfPayAmount();
+		// 今日手续费收入
+		BigDecimal todayDfOrderFee = mapper.summaryTodayDfOrderFee();
+		map.put("userAmount", userAmount);
+		map.put("payAmount", payAmount);
+		map.put("orderFee", orderFee);
+		map.put("todayRechargeAmount", todayRechargeAmount);
+		map.put("todayDfPayAmount", todayDfPayAmount);
+		map.put("todayDfOrderFee", todayDfOrderFee);
 
-  public Map<String, Object> agentSummary(String userId) {
-    Map map = new HashMap();
-    // 剩余额度
-    BigDecimal userAmount = mapper.summaryUserAmount(userId);
-    // 已代付总额
-    BigDecimal payAmount = mapper.summaryDfPayAmount(userId);
-    // 手续费收入
-    BigDecimal orderFee = mapper.summaryDfOrderFee(userId);
-    // 今日充值
-    BigDecimal todayRechargeAmount = mapper.summaryTodayRechargeAmount(userId);
-    // 今日代付
-    BigDecimal todayDfPayAmount = mapper.summaryTodayDfPayAmount(userId);
-    // 今日手续费收入
-    BigDecimal todayDfOrderFee = mapper.summaryTodayDfOrderFee(userId);
-    map.put("userAmount", userAmount);
-    map.put("payAmount", payAmount);
-    map.put("orderFee", orderFee);
-    map.put("todayRechargeAmount", todayRechargeAmount);
-    map.put("todayDfPayAmount", todayDfPayAmount);
-    map.put("todayDfOrderFee", todayDfOrderFee);
+		return map;
+	}
 
-    return map;
-  }
+	public Map<String, Object> agentSummary(String userId) {
+		Map map = new HashMap();
+		// 剩余额度
+		BigDecimal userAmount = mapper.summaryUserAmount(userId);
+		// 已代付总额
+		BigDecimal payAmount = mapper.summaryDfPayAmount(userId);
+		// 手续费收入
+		BigDecimal orderFee = mapper.summaryDfOrderFee(userId);
+		// 今日充值
+		BigDecimal todayRechargeAmount = mapper.summaryTodayRechargeAmount(userId);
+		// 今日代付
+		BigDecimal todayDfPayAmount = mapper.summaryTodayDfPayAmount(userId);
+		// 今日手续费收入
+		BigDecimal todayDfOrderFee = mapper.summaryTodayDfOrderFee(userId);
+		map.put("userAmount", userAmount);
+		map.put("payAmount", payAmount);
+		map.put("orderFee", orderFee);
+		map.put("todayRechargeAmount", todayRechargeAmount);
+		map.put("todayDfPayAmount", todayDfPayAmount);
+		map.put("todayDfOrderFee", todayDfOrderFee);
 
-  public Map<String, Object> userSummary(String userId) {
-    Map map = new HashMap();
-    // 剩余额度
-    BigDecimal userAmount = mapper.getUserAmount(userId);
-    // 已代付总额
-    BigDecimal payAmount = mapper.summaryUserDfPayAmount(userId);
-    // 手续费收入
-    BigDecimal orderFee = mapper.summaryUserDfOrderFee(userId);
-    // 今日充值
-    BigDecimal todayRechargeAmount = mapper.summaryUserTodayRechargeAmount(userId);
-    // 今日代付
-    BigDecimal todayDfPayAmount = mapper.summaryUserTodayDfPayAmount(userId);
-    // 今日手续费收入
-    BigDecimal todayDfOrderFee = mapper.summaryUserTodayDfOrderFee(userId);
-    map.put("userAmount", userAmount);
-    map.put("payAmount", payAmount);
-    map.put("orderFee", orderFee);
-    map.put("todayRechargeAmount", todayRechargeAmount);
-    map.put("todayDfPayAmount", todayDfPayAmount);
-    map.put("todayDfOrderFee", todayDfOrderFee);
-    return map;
-  }
+		return map;
+	}
+
+	public Map<String, Object> userSummary(String userId) {
+		Map map = new HashMap();
+		// 剩余额度
+		BigDecimal userAmount = mapper.getUserAmount(userId);
+		// 已代付总额
+		BigDecimal payAmount = mapper.summaryUserDfPayAmount(userId);
+		// 手续费收入
+		BigDecimal orderFee = mapper.summaryUserDfOrderFee(userId);
+		// 今日充值
+		BigDecimal todayRechargeAmount = mapper.summaryUserTodayRechargeAmount(userId);
+		// 今日代付
+		BigDecimal todayDfPayAmount = mapper.summaryUserTodayDfPayAmount(userId);
+		// 今日手续费收入
+		BigDecimal todayDfOrderFee = mapper.summaryUserTodayDfOrderFee(userId);
+		map.put("userAmount", userAmount);
+		map.put("payAmount", payAmount);
+		map.put("orderFee", orderFee);
+		map.put("todayRechargeAmount", todayRechargeAmount);
+		map.put("todayDfPayAmount", todayDfPayAmount);
+		map.put("todayDfOrderFee", todayDfOrderFee);
+		return map;
+	}
 }
