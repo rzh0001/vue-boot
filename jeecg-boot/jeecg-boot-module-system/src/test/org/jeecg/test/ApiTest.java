925c94d20f79d96b7186f0d2e9e5e3dfd9fce2b6
rzh0001@qq.com <rzh0001@qq.com>
2020-03-10 18:20:39 +0800

    更新接口测试 代码

--- jeecg-boot/jeecg-boot-module-system/src/test/org/jeecg/test/ApiTest.java
+++ jeecg-boot/jeecg-boot-module-system/src/test/org/jeecg/test/ApiTest.java
@@ -4,52 +4,80 @@ import cn.hutool.http.HttpUtil;
 import org.jeecg.modules.df.entity.ApiRequestBody;
 import org.jeecg.modules.df.entity.PayOrderData;
 import org.jeecg.modules.df.entity.QueryOrderData;
+import org.junit.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Set;
 
 /**
  * @author ruanzh
  * @since 2019/11/17
  */
 public class ApiTest {
-    
-    public static void main(String[] args) {
-        //
-        PayOrderData data = new PayOrderData();
-        data.setAccountType("1");
-        data.setAccountName("王二");
-        data.setAccountNo("1231241241");
-        data.setProductCode("alipay");
-        data.setBizOrderNo("3333333sss333333333");
-        data.setAmount(BigDecimal.TEN);
-        data.setCallbackUrl("q3");
-        data.setIp("1.1.1.1");
-        data.setRemark("sssssss");
-        data.setBankCode("31211212211");
-        
-        String apiKey = "53e6f5cc060a4adf";
-    
-        ApiRequestBody req = new ApiRequestBody();
-        req.setUsername("shanghu");
-        req.setTimestamp(System.currentTimeMillis());
-        req.setData(data.encodeData(apiKey));
-        req.setSign(req.sign(apiKey));
-        System.out.println(req.toJsonString());
-    
-        QueryOrderData query = new QueryOrderData();
-        query.setBizOrderNo(data.getBizOrderNo());
-        req.setData(query.encodeData(apiKey));
-        req.setSign(req.sign(apiKey));
-        System.out.println(req.toJsonString());
-    
-    
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("city", "北京");
-    
-        String result = HttpUtil.post("https://www.baidu.com", paramMap);
-        System.out.println(result);
-    
-    
-    }
+
+	private String username = "user_test";       // 对接测试帐号
+	private String apiKey = "1cd31af85fd94f76";  // 对接测试帐号 密钥
+
+	//	private String ip = "http://47.244.49.200:8080";
+	private String ip = "http://127.0.0.1:8080";
+	private String createUrl = ip + "/dfxt/api/order/create";
+	private String queryUrl = ip + "/dfxt/api/order/query";
+
+	private String orderId = "df20200120101012127827"; // 您系统的订单号，测试时可以更换
+
+	/**
+	 * 创建订单接口
+	 */
+	@Test
+	void createOrder() {
+		PayOrderData data = new PayOrderData();
+		data.setAccountType("1222");
+		data.setAccountName("王二");
+		data.setAccountNo("1231241241");
+		data.setProductCode("alipay");
+		data.setBankName("快乐银行");
+		data.setBranchName("天上人间分行");
+		data.setBizOrderNo(orderId);
+		data.setAmount(BigDecimal.TEN);
+		data.setCallbackUrl("q3"); //回调地址，若您的系统无需回调，请勿赋值
+		data.setIp("1.1.1.1");
+		data.setRemark("sssssss");
+		data.setBankCode("31211212211");
+
+
+		ApiRequestBody req = new ApiRequestBody();
+		req.setUsername(username);
+		req.setTimestamp(System.currentTimeMillis());
+		req.setData(data.encodeData(apiKey));
+		req.setSign(req.sign(apiKey));
+
+		System.out.println("请求信息：" + req.toJsonString());
+		String result = HttpUtil.post(createUrl, req.toJsonString());
+		System.out.println("返回信息：" + result);
+	}
+
+	/**
+	 * 订单查询接口
+	 */
+	@Test
+	void queryOrder() {
+		QueryOrderData query = new QueryOrderData();
+		query.setBizOrderNo(orderId);
+
+		ApiRequestBody req = new ApiRequestBody();
+		req.setUsername(username);
+		req.setTimestamp(System.currentTimeMillis());
+		req.setData(query.encodeData(apiKey));
+		req.setSign(req.sign(apiKey));
+
+		System.out.println("请求信息：" + req.toJsonString());
+		String result = HttpUtil.post(queryUrl, req.toJsonString());
+		System.out.println("返回信息：" + result);
+	}
+
 }
