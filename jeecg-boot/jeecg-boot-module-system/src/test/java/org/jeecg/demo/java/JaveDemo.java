fc936bc283c63d3b4a5d672d41b65c2f2d647058
rzh0001@qq.com <rzh0001@qq.com>
2020-03-25 23:46:18 +0800

    fix

--- jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/demo/java/JaveDemo.java
+++ jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/demo/java/JaveDemo.java
@@ -19,63 +19,69 @@ import java.util.Date;
  * @create: 2019-08-21 16:53
  */
 public class JaveDemo {
-    /**
-     * 创建订单
-     */
-    @Test
-    public void create() throws Exception {
-        JSONObject req = new JSONObject();
-        JSONObject data = new JSONObject();
-        data.put(BaseConstant.OUTER_ORDER_ID,"1565776105000abc");
-        data.put(BaseConstant.USER_NAME,"www");
-        data.put(BaseConstant.SUBMIT_AMOUNT,"111");
-        data.put(BaseConstant.PAY_TYPE,"ysf");
-        data.put(BaseConstant.CALLBACK_URL,"http://localhost/api/callback");
-        //加密数据
-        String dataEn = AES128Util.encryptBase64(data.toJSONString(), "1234123412ABCDEF");
-        StringBuilder sign = new StringBuilder();
-        //userId+timestamp+data
-        Long time = new Date().getTime();
-        sign.append("www").append(time).append(dataEn).append("1234123412ABCDEF");
+	/**
+	 * 创建订单
+	 */
+	@Test
+	public void create() throws Exception {
+		JSONObject req = new JSONObject();
+		JSONObject data = new JSONObject();
+		data.put(BaseConstant.OUTER_ORDER_ID, "1565776105000abc");
+		data.put(BaseConstant.USER_NAME, "www");
+		data.put(BaseConstant.SUBMIT_AMOUNT, "111");
+		data.put(BaseConstant.PAY_TYPE, "ysf");
+		data.put(BaseConstant.CALLBACK_URL, "http://localhost/api/callback");
+		//加密数据
+		String dataEn = AES128Util.encryptBase64(data.toJSONString(), "1234123412ABCDEF");
+		StringBuilder sign = new StringBuilder();
+		//userId+timestamp+data
+		Long time = new Date().getTime();
+		sign.append("www").append(time).append(dataEn).append("1234123412ABCDEF");
 
-        req.put(BaseConstant.SIGN, DigestUtils.md5Hex(sign.toString()));
-        req.put(BaseConstant.TIMESTAMP,time);
-        req.put(BaseConstant.DATA,dataEn);
-        req.put(BaseConstant.USER_NAME,"www");
-        System.out.println(req.toJSONString());
-        HttpResult result = HttpUtils.doPostJson("http://www.jcokpay.com/jeecg-boot/api/create",req.toJSONString());
-        JSONObject r = JSON.parseObject(result.getBody());
-        if("0".equals(r.get("code").toString())){
-            String payUrl = r.get("url").toString();
-        }
-    }
+		req.put(BaseConstant.SIGN, DigestUtils.md5Hex(sign.toString()));
+		req.put(BaseConstant.TIMESTAMP, time);
+		req.put(BaseConstant.DATA, dataEn);
+		req.put(BaseConstant.USER_NAME, "www");
+		System.out.println(req.toJSONString());
+		HttpResult result = HttpUtils.doPostJson("http://www.jcokpay.com/jeecg-boot/api/create", req.toJSONString());
+		JSONObject r = JSON.parseObject(result.getBody());
+		if ("0".equals(r.get("code").toString())) {
+			String payUrl = r.get("url").toString();
+		}
+	}
 
-    /**
-     * 订单查询
-     * @throws Exception
-     */
-    @Test
-    public void queryOrderInfo() throws Exception {
-        JSONObject data = new JSONObject();
-        data.put(BaseConstant.ORDER_ID,"20190817172444SIGKq");
-        String dataEn = AES128Util.encryptBase64(data.toJSONString(), "1234123412ABCDEF");
-        JSONObject req = new JSONObject();
-        Long time = new Date().getTime();
-        req.put(BaseConstant.USER_NAME,"www");
-        req.put(BaseConstant.DATA,dataEn);
-        req.put(BaseConstant.TIMESTAMP,time);
-        StringBuilder sign = new StringBuilder();
-        sign.append("www").append(time).append(dataEn).append("1234123412ABCDEF");
-        req.put(BaseConstant.SIGN, DigestUtils.md5Hex(sign.toString()));
-        HttpResult result = HttpUtils.doPostJson("http://www.jcokpay.com/jeecg-boot/api/queryOrder",req.toJSONString());
-        JSONObject r = JSON.parseObject(result.getBody());
-        if("0".equals(r.get("code").toString())){
-            String info = r.get("orderInfo").toString();
-            JSONObject orderInfo = JSON.parseObject(info);
-            orderInfo.get("status");
-            orderInfo.get("submitAmount");
-            orderInfo.get("orderId");
-            orderInfo.get("outerOrderId");
-        }
-    }
+	/**
+	 * 订单查询
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void queryOrderInfo() throws Exception {
+		String username = "NJ08";
+		String apiKey = "3c6a3ce41ec44a6c";
+		String orderId = "20200323192459gAAyC";
+		JSONObject data = new JSONObject();
+		data.put(BaseConstant.ORDER_ID, orderId);
+		String dataEn = AES128Util.encryptBase64(data.toJSONString(), apiKey);
+		System.out.println(dataEn);
+
+		JSONObject req = new JSONObject();
+		Long time = new Date().getTime();
+		req.put(BaseConstant.USER_NAME, username);
+		req.put(BaseConstant.DATA, dataEn);
+		req.put(BaseConstant.TIMESTAMP, time);
+		StringBuilder sign = new StringBuilder();
+		sign.append(username).append(time).append(dataEn).append(apiKey);
+		req.put(BaseConstant.SIGN, DigestUtils.md5Hex(sign.toString()));
+//		HttpResult result = HttpUtils.doPostJson("http://www.jcokpay.com/jeecg-boot/api/queryOrder", req.toJSONString());
+//		JSONObject r = JSON.parseObject(result.getBody());
+//		if ("0".equals(r.get("code").toString())) {
+//			String info = r.get("orderInfo").toString();
+//			JSONObject orderInfo = JSON.parseObject(info);
+//			orderInfo.get("status");
+//			orderInfo.get("submitAmount");
+//			orderInfo.get("orderId");
+//			orderInfo.get("outerOrderId");
+//		}
+	}
 }
