1e2e1f86e76b70e23d0d4a99e17adbacc2298dba
rzh0001@qq.com <rzh0001@qq.com>
2020-05-05 19:36:40 +0800

    测试代码

--- jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/apiv2/ApiV2Test.java
+++ jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/apiv2/ApiV2Test.java
@@ -1,8 +1,10 @@
 package org.jeecg.apiv2;
 
 import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSONObject;
+import org.jeecg.common.util.encryption.AES128Util;
 import org.jeecg.modules.api.entity.ApiRequestBody;
-import org.jeecg.modules.api.entity.PayOrderData;
+import org.jeecg.modules.api.entity.PayOrderRequestData;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,17 +15,15 @@ public class ApiV2Test {
 	private String username = "阿贵002";       // 对接测试帐号
 	private String apiKey = "77f8a1ef73bb4b84";  // 对接测试帐号 密钥
 
-	//	private String ip = "http://47.52.0.162";
 	private String ip = "http://127.0.0.1:8080";
 	private String createUrl = ip + "/pay/api/v2/order/create";
-	private String queryUrl = ip + "/dfxt/api/order/query";
-	private String callbackUrl = ip + "/pay/api/v2/order/callback";
+	private String queryUrl = ip + "/pay/api/v2/order/query/";
 
-	private String orderId = "df202001246546rw827"; // 您系统的订单号，测试时可以更换
+	private String orderId = "df2020e012246546rw827"; // 您系统的订单号，测试时可以更换
 
 	@Test
 	public void create() {
-		PayOrderData data = new PayOrderData();
+		PayOrderRequestData data = new PayOrderRequestData();
 		data.setOuterOrderId(orderId);
 		data.setProductCode("AlipayScan");
 		data.setAmount(new BigDecimal(400));
@@ -45,7 +45,17 @@ public class ApiV2Test {
 	}
 
 	@Test
-	public void callback() {
-		String result = HttpUtil.post(callbackUrl + "/ali_bank", "{'asdf': 'asdf'}");
+	public void query() {
+		JSONObject json = new JSONObject();
+		json.put("outerOrderId", orderId);
+		ApiRequestBody req = new ApiRequestBody();
+		req.setUsername(username);
+		req.setTimestamp(System.currentTimeMillis());
+		req.setData(AES128Util.encryptBase64(json.toJSONString(), apiKey));
+		req.setSign(req.sign(apiKey));
+
+		System.out.println("请求信息：" + req.toJsonString());
+		String result = HttpUtil.post(queryUrl + orderId, req.toJsonString());
+		System.out.println("返回信息：" + result);
 	}
 }
