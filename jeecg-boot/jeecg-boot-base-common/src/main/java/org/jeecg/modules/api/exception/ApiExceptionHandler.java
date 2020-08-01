c09da0b598dbff917444b2d35a8a9779a383bd04
rzh0001@qq.com <rzh0001@qq.com>
2020-08-01 23:58:09 +0800

    修改ExceptionHandler执行顺序，修复接口返回格式错误

--- jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/modules/api/exception/ApiExceptionHandler.java
+++ jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/modules/api/exception/ApiExceptionHandler.java
@@ -2,7 +2,7 @@ package org.jeecg.modules.api.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jeecg.modules.api.entity.ApiResponseBody;
-import org.jeecg.modules.api.exception.ApiException;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,16 +11,18 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * API异常处理器
+ *
  * @author ruanzh
  * @since 2019/11/17
  */
+@Order(1)
 @RestControllerAdvice
 @Slf4j
 public class ApiExceptionHandler {
-    @ExceptionHandler(value = ApiException.class)
-    @ResponseBody
-    public ApiResponseBody defaultErrorHandler(HttpServletRequest req, ApiException e) throws Exception {
-        log.error(e.getMsg());
-        return ApiResponseBody.error(e.getCode(), e.getMsg());
-    }
+	@ExceptionHandler(value = ApiException.class)
+	@ResponseBody
+	public ApiResponseBody defaultErrorHandler(HttpServletRequest req, ApiException e) throws Exception {
+		log.error(e.getMsg());
+		return ApiResponseBody.error(e.getCode(), e.getMsg());
+	}
 }
